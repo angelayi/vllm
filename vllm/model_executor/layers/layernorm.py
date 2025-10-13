93a55a73fe1212df2ca2202125234ca6ab066678
angelayi <yiangela7@gmail.com>
2025-10-13 00:25:53 -0700

    [tmp] Fix typing

--- vllm/model_executor/layers/layernorm.py
+++ vllm/model_executor/layers/layernorm.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Custom normalization layers."""
+from typing import Union, Optional 
 
 import torch
 import torch.nn as nn
